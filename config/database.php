bca00f69146b5a7b5e02ee8f45d57ceb8f91ea60
nicerazer <amocuk@gmail.com>
2020-05-21 05:45:33 +0800

    HEROKU: Bunch of stuff

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+// Heroku postgresql service
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
 return [
 
     /*
@@ -63,19 +66,26 @@ return [
             ]) : [],
         ],
 
+        // Heroku postgresql service
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL["port"],
+            // 'port' => env('DB_PORT', '5432'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            'username' => $DATABASE_URL["user"],
+            // 'username' => env('DB_USERNAME', 'forge'),
+            'password' => $DATABASE_URL["pass"],
+            // 'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
+            // 'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
